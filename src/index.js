19564df1e7885fe677c185fd40a750d64e3eed84
John <hostejj@gmail.com>
2021-12-13 15:36:48 -0500

    Adding toolbar code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
+import { Toolbar } from './ui-components';
 
 ReactDOM.render(
   <React.StrictMode>
