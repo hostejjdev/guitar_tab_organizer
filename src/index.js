dce5a953d4fb9af8a6e3df5167a3c3ef55d929d2
John <hostejj@gmail.com>
2021-12-13 16:02:25 -0500

    audding auth

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
+import config from './aws-exports';
+Amplify.configure(config);
 Amplify.configure(awsExports);
 
 ReactDOM.render(
